f62df5b4057850a804170e677db7c7a9f8e4b557
Martin Petersson <martin.petersson@emsbk.com>
2019-12-09 16:28:05 +0100

    Missing day_02.cs

---
+++ 2019/AdventOfCode2019/Day_02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    public class Day_02 : Shared
+    {
+        public string Day_02_Part1()
+        {
+            var opcodes = GetInput().Split(',').Select(x => int.Parse(x)).ToArray();
+            opcodes[1] = 12;
+            opcodes[2] = 2;
+            for (int i = 0; i < opcodes.Length; i++)
+            {
+                if (i % 4 != 0) continue;
+                if (opcodes[i] == 99) break;
+                var a = opcodes[i + 0];   // opcode
+                var b = opcodes[i + 1];   // value 1
+                var c = opcodes[i + 2];   // value 2
+                var d = opcodes[i + 3];   // position
+                opcodes[d] = a switch
+                {
+                    1 => opcodes[b] + opcodes[c],
+                    2 => opcodes[b] * opcodes[c],
+                    _ => throw new Exception("Invalid opcode!"),
+                };
+            }
+
+            return Result(opcodes[0]);
+        }
+    }
+}
