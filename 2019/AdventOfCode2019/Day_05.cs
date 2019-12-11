c6afb4485d9ef0e0adcc07a92aac45558fe8d816
Kagemand Andersen <trillykins@gmail.com>
2019-12-11 21:13:21 +0100

    Checkpoint

--- 2019/AdventOfCode2019/Day_05.cs
+++ 2019/AdventOfCode2019/Day_05.cs
@@ -19,9 +19,9 @@ namespace AdventOfCode2019
         private string Day_05_Part1()
         {
             var input = GetAllInput().Split(',').Select(x => int.Parse(x)).ToArray();
-            //RunInstructions(new int[] { 3, 0, 4, 0, 99 });  // Test case
-            RunInstructions(input);  // Test case
-            return Result(0);
+            //var result = RunInstructions(new int[] { 3, 0, 4, 0, 99 });  // Test case
+            var result = RunInstructions(input);  // Test case
+            return Result(result);
         }
 
         private string Day_05_Part2()
@@ -33,41 +33,52 @@ namespace AdventOfCode2019
         private int RunInstructions(int[] opcodes)
         {
             int i = 0;
+            var output = new List<int>();
             while (true)
             {
                 var instructionPointer = 2;
                 if (opcodes[i] == 99) break;
                 var a = opcodes[i + 0];   // opcode
-                var str = a.ToString();
+                var b = opcodes[i + 1];   // parameter 1
                 if (a == 1 || a == 2)
                 {
-                    var b = opcodes[i + 1];   // value 1
-                    var c = opcodes[i + 2];   // value 2
-                    var d = opcodes[i + 3];   // position
-                    opcodes[d] = a switch
-                    {
-                        1 => opcodes[b] + opcodes[c],
-                        2 => opcodes[b] * opcodes[c],
-                        _ => throw new Exception("Invalid opcode!"),
-                    };
+                    opcodes = AddOrMultiply(opcodes, i, a, new int[] { 0, 0 });
                     instructionPointer = 4;
                 }
                 else if (a == 3)
                 {
-                    var b = opcodes[i + 1];
                     opcodes[b] = 1;
                 }
                 else if (a == 4)
                 {
-                    var b = opcodes[i + 1];
-                    Console.WriteLine(opcodes[b]);
+                    output.Add(opcodes[b]);
                 }
-                else if (str.Length > 1)
+                else if (a.ToString().Length > 1)
                 {
                     // e.g. 1101
+                    var str = a.ToString();
                     if (str.Length == 2) throw new ArgumentOutOfRangeException($"{str} doesn't really make sense, does it?");
-                    str = a.ToString("00000");
-                    // do stuff
+                    str = a.ToString("0000");
+                    var opcode = int.Parse(str.Substring(2));
+                    var parameterModes = str.Substring(0, 2).ToCharArray().Select(x => int.Parse($"{x}")).ToArray();
+                    if (opcode == 1 || opcode == 2)
+                    {
+                        AddOrMultiply(opcodes, i, opcode, parameterModes);
+                        instructionPointer = 4;
+                    }
+                    else if (opcode == 3)
+                    {
+                        opcodes[b] = 1;
+                    }
+                    else if (opcode == 4)
+                    {
+                        if (parameterModes.Any(x => x == 1)) output.Add(b);
+                        else output.Add(opcodes[b]);
+                    }
+                    else
+                    {
+                        throw new NotSupportedException();
+                    }
                 }
                 else
                 {
@@ -76,7 +87,28 @@ namespace AdventOfCode2019
                 if (instructionPointer != 4 && instructionPointer != 2) throw new Exception("Bad instruction!");
                 i += instructionPointer;
             }
-            return opcodes[0];
+            return output.Last();
+        }
+
+        private int[] AddOrMultiply(int[] opcodes, int i, int a, int[] modes)
+        {
+            var b = opcodes[i + 1];   // value 1
+            var c = opcodes[i + 2];   // value 2
+            var d = opcodes[i + 3];   // position
+            opcodes[d] = a switch
+            {
+                1 => PositionOrImmediate(opcodes, b, modes[0]) + PositionOrImmediate(opcodes, c, modes[1]),
+                2 => PositionOrImmediate(opcodes, b, modes[0]) * PositionOrImmediate(opcodes, c, modes[1]),
+                _ => throw new Exception("Invalid opcode!"),
+            };
+            return opcodes;
+        }
+
+        private int PositionOrImmediate(int[] opcodes, int value, int decider)
+        {
+            if (decider == 0) return opcodes[value];    // Position
+            if (decider == 1) return value;             // Immediate
+            throw new NotSupportedException();
         }
     }
 }
